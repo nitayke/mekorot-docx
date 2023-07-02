a7bb2028aa3bbbbb9de13307f07167fd64ffb589
nitayke <nitayke@gmail.com>
2023-07-02 13:50:28 +0300

    added constants

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import {
   FrameAnchorType,
   AlignmentType,
   BorderStyle,
+  ColumnBreak,
 } from "docx";
 import { saveAs } from "file-saver";
 import "./App.css";
@@ -25,13 +26,18 @@ const BORDERS = {
 };
 
 const MARGIN = 300;
-const WIDTH_SUM = 11100;
+const WIDTH_SUM = 11050;
 const FONT = "David";
+const CHARS_IN_LINE = 128;
+const LINE_HEIGHT = 230;
+const VERTICAL_SPACE = 150;
+const HORIZONTAL_SPACE = 250;
+const MAX_RATIO = 2.5; // If the ratio between 2 mekorot is below 2.5, I put them side-by-side
 
 function generateMekorotList() {
   const EXAMPLE_MAKOR = `אבל בפני ע"א לא אמרינן דאיתרע שטרא. ואינו דומה לנסכא דר"א דשם הוי מחויב שבועה דאורייתא ואיל"מ מה שאין כן זה שאין לו אלא שבועת המשנה בע"א שהוא פרוע לכן ל"א בי' מתוך כ"כ בעל התרומות בשם הרמב"ן שהשיב לו כך. והנה ראיתי כל הגדולים וחשובים יוצאים לקראת משה רבינו והקשו דברי הרמב"ן אהדדי דכאן דעתו דבע"א לא אתרע שטרא ולא אמרינן בי' מתוך ובפרק חזקת הבתים דף ל"ד גבי נסכא דר"א כתב להדיא דאבימי קודם שנתקנה שבועת היסת דכיון דאי הוי מכחיש לשליח הי' צריך לישבע להכחיש את השליח לא הוי מגו ואתרע שטרא וכ"כ תוספות בעובדא דאבימי בפ' הכותב והנה בש"ך כתב עצה נכונה ליישב והוא דהתוספות והרמב"ן מיירי בתר פרעון דאם פקח הוא מייתי לידי שבועה דאורייתא ובתר פרעון כיון דהוי לי' ש"ד הוי לי' מחויב שואיל"מ ועיין שם דמותיב אנפשי' מאי הקשו מעובד' דאבימי דילמא מיירי עובדא דאבימי קודם פרעון ועיין שם אלא דסתימת דברי הרמ"א משמע דלעולם לא איתרע שטרא בפני ע"א דאם לא כן ה"ל לפרש דבתר פרעון צריך לחזור משום דהוי לי' מתוך שאינו יכול לישבע משלם ועוד דאם כן הפוכי מטרתי למה כיון דבתר פרעון יצטרך להחזיר למה יפרע. ולכן נראה דבתר פרעון לא שייך דין מתוך כיון דלדעת הרמ"א אפילו בשני עדים מעידים על הפרעון בסתם דאינו יכול לטעון סטראי אבל אי תפס לא מפקינן מיני' ואם כן ודאי בתר פרעון לא שייך דין מתוך שיצטרך לחזור המעות דהא אפילו שני עדים מעידים על פרעון סתם יכול לומר סטראי ואין צריך המגו כלל רק להוציא אבל להחזיק מה שתפוס ועומד סטראי מחמת עצמה טענה אפילו בשני עדים וז"ב. ואפילו לדעת השלחן ערוך דל"מ תפיסה לטענת סטראי היכא דאי' שני' על הפרעון סתם היינו משום דהאי שטרא איתרע לגמרי ואינו נאמן לתפוס על חוב שבשטר דכבר איתרע אבל תביעת ממון ודאי ליכא דהא מבואר בשלחן ערוך סימן ע"ט אמר לו מנה הלויתיך בפני פ' ופ' וכו' אבל טען אמת שקבלתי מנה אבל בפרעון חובי נאמן והרי משמע להדיא דאין בזה משום תביעת ממון אלא הא דאיתרע שטרו ואינו נאמן בתפיס' היינו משום דהאי שטרא איתרע לגמרי וכנ"ל ואם כן גבי אי פקת הוא דמייתי לידי ש"ד היינו דמוקים לי' לקמאי בהלואה דע"א אינו נאמן לארועי שטרא וכמ"ש בש"ך סימן עיין דאינו אלא משום פלגינן דיבורי' ואמרינן דמוקי' לה בהלואה. וכיון דהעיד בסתמא ל"מ דעל מנה סתם מצי אמר לפרעון חובו. ואם כן בתר פרעון אפילו לדעת ש"ע לא אמרינן משואיל"מ וכן מוכח מתשובת הרא"ש כלל פ"ו דאם על חוב שבשטר הוא בא בהרשאה ועל הממון שהודה שקיבל הוא בא מכח עצמו דאי בעי ון ודו"ק:`;
   let mekorot = [];
-  for (let i = 0; i < 7; i++) {
+  for (let i = 0; i < 9; i++) {
     let start = randomInt(EXAMPLE_MAKOR.length);
     let end = start + randomInt(EXAMPLE_MAKOR.length - start);
     let currentString = EXAMPLE_MAKOR.slice(start, end);
@@ -69,6 +75,7 @@ function newTextBox(x, y, w, h, text) {
         size: 22,
         rightToLeft: true,
       }),
+      new ColumnBreak(),
       new TextRun({
         break: 1,
         text: text,
@@ -80,42 +87,63 @@ function newTextBox(x, y, w, h, text) {
   });
 }
 
+function locateSideBySide(textBoxes, ratio, first, second, currPosition) {
+  let width = (ratio / (ratio + 1)) * WIDTH_SUM;
+  let chars_in_curr_line = [
+    (width / WIDTH_SUM) * CHARS_IN_LINE - 3,
+    ((WIDTH_SUM - width) / WIDTH_SUM) * CHARS_IN_LINE - 3,
+  ];
+  let lines_count =
+    Math.max(
+      Math.floor(first.length / chars_in_curr_line[0]),
+      Math.floor(second.length / chars_in_curr_line[1])
+    ) + 2;
+  let height = lines_count * LINE_HEIGHT;
+  textBoxes.push(newTextBox(0, currPosition, width, height, first));
+  textBoxes.push(
+    newTextBox(
+      width + HORIZONTAL_SPACE,
+      currPosition,
+      WIDTH_SUM - width,
+      height,
+      second
+    )
+  );
+  return height;
+}
+
+function locateSingle(textBoxes, makor, currPosition) {
+  let lines_count = Math.floor(makor.length / CHARS_IN_LINE) + 2;
+  let height = lines_count * LINE_HEIGHT;
+  console.log(lines_count, height);
+  textBoxes.push(
+    newTextBox(0, currPosition, WIDTH_SUM + HORIZONTAL_SPACE, height, makor)
+  );
+  return height;
+}
+
 function generate() {
   let currentPosition = 0;
   let mekorot = generateMekorotList();
   let textBoxes = [];
+  let height;
   for (let i = 0; i < mekorot.length - 1; i++) {
+    if (i === mekorot.length - 1)
+      locateSingle(textBoxes, mekorot[i], currentPosition);
     let ratio = mekorot[i].length / mekorot[i + 1].length;
-    if (ratio < 2 && ratio > 0.5) {
-      let height = (mekorot[i].length + mekorot[i + 1].length) * 3;
-      let width = (ratio / (ratio + 1)) * WIDTH_SUM;
-      textBoxes.push(
-        newTextBox(0, currentPosition, width, undefined, mekorot[i])
+    if (ratio < MAX_RATIO && ratio > 1 / MAX_RATIO) {
+      height = locateSideBySide(
+        textBoxes,
+        ratio,
+        mekorot[i],
+        mekorot[i + 1],
+        currentPosition
       );
-      textBoxes.push(
-        newTextBox(
-          width + MARGIN,
-          currentPosition,
-          WIDTH_SUM - width,
-          undefined, // NEED TO FIND SOLUTION TO THIS
-          mekorot[i + 1]
-        )
-      );
-      currentPosition += height + MARGIN;
       i++;
     } else {
-      let height = mekorot[i].length * 5;
-      textBoxes.push(
-        newTextBox(
-          0,
-          currentPosition,
-          WIDTH_SUM + MARGIN,
-          undefined,
-          mekorot[i]
-        )
-      );
-      currentPosition += height + MARGIN;
+      height = locateSingle(textBoxes, mekorot[i], currentPosition);
     }
+    currentPosition += height + VERTICAL_SPACE;
   }
 
   const doc = new Document({
